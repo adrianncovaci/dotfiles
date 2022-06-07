c90673aac88b0a23a14ff167b25b0b73e268f280
adrianncovaci <adrianmihaicovaci@gmail.com>
2022-06-08 00:11:48 +0300

    modified dap for rust and lsp - tnx prime

--- nvim/lua/daprust.lua
+++ nvim/lua/daprust.lua
@@ -4,10 +4,26 @@ vim.keymap.set("n", "<F11>", ":lua require'dap'.step_into()<CR>")
 vim.keymap.set("n", "<F12>", ":lua require'dap'.step_out()<CR>")
 vim.keymap.set("n", "<leader>b", ":lua require'dap'.toggle_breakpoint()<CR>")
 vim.keymap.set("n", "<leader>B", ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")
-vim.keymap.set("n", "<leader>B", ":lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>")
+vim.keymap.set("n", "<leader>drb", ":lua require'dap'.clear_breakpoints()<CR>")
+vim.keymap.set("n", "<leader>lp", ":lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>")
 vim.keymap.set("n", "<leader>dr", ":lua require'dap'.repl.open()<CR>")
+require("nvim-dap-virtual-text").setup()
 
 local dap = require('dap')
+local dapui = require('dapui')
+
+dap.listeners.after.event_initialized["dapui_config"] = function()
+    dapui.open()
+end
+
+dap.listeners.before.event_terminated["dapui_config"] = function()
+    dapui.close()
+end
+
+dap.listeners.before.event_exited["dapui_config"] = function()
+    dapui.close()
+end
+
 dap.adapters.lldb = {
   type = 'executable',
   command = '/usr/bin/lldb-vscode', -- adjust as needed, must be absolute path
--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,3 +1,116 @@
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+-- Setup nvim-cmp.
+local cmp = require("cmp")
+local source_mapping = {
+	buffer = "[Buffer]",
+	nvim_lsp = "[LSP]",
+	nvim_lua = "[Lua]",
+	cmp_tabnine = "[TN]",
+	path = "[Path]",
+}
+local lspkind = require("lspkind")
+
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			-- For `vsnip` user.
+			-- vim.fn["vsnip#anonymous"](args.body)
+
+			-- For `luasnip` user.
+			require("luasnip").lsp_expand(args.body)
+
+			-- For `ultisnips` user.
+			-- vim.fn["UltiSnips#Anon"](args.body)
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+		["<C-u>"] = cmp.mapping.scroll_docs(-4),
+		["<C-d>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+	}),
+
+	formatting = {
+		format = function(entry, vim_item)
+			vim_item.kind = lspkind.presets.default[vim_item.kind]
+			local menu = source_mapping[entry.source.name]
+			if entry.source.name == "cmp_tabnine" then
+				if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then
+					menu = entry.completion_item.data.detail .. " " .. menu
+				end
+				vim_item.kind = ""
+			end
+			vim_item.menu = menu
+			return vim_item
+		end,
+	},
+
+	sources = {
+		-- tabnine completion? yayaya
+
+		{ name = "cmp_tabnine" },
+
+		{ name = "nvim_lsp" },
+
+		-- For vsnip user.
+		-- { name = 'vsnip' },
+
+		-- For luasnip user.
+		{ name = "luasnip" },
+
+		-- For ultisnips user.
+		-- { name = 'ultisnips' },
+
+		{ name = "buffer" },
+	},
+})
+
+local tabnine = require("cmp_tabnine.config")
+tabnine:setup({
+	max_lines = 1000,
+	max_num_results = 20,
+	sort = true,
+	run_on_every_keystroke = true,
+	snippet_placeholder = "..",
+})
+
+local function config(_config)
+	return vim.tbl_deep_extend("force", {
+		capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+		on_attach = function()
+			Nnoremap("gd", ":lua vim.lsp.buf.definition()<CR>")
+			Nnoremap("K", ":lua vim.lsp.buf.hover()<CR>")
+			Nnoremap("<leader>vws", ":lua vim.lsp.buf.workspace_symbol()<CR>")
+			Nnoremap("<leader>vd", ":lua vim.diagnostic.open_float()<CR>")
+			Nnoremap("[d", ":lua vim.lsp.diagnostic.goto_next()<CR>")
+			Nnoremap("]d", ":lua vim.lsp.diagnostic.goto_prev()<CR>")
+			Nnoremap("<leader>vca", ":lua vim.lsp.buf.code_action()<CR>")
+			Nnoremap("<leader>vrr", ":lua vim.lsp.buf.references()<CR>")
+			Nnoremap("<leader>vrn", ":lua vim.lsp.buf.rename()<CR>")
+			Inoremap("<C-h>", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
+		end,
+	}, _config or {})
+end
+
+require("lspconfig").tsserver.setup(config())
+
+--[[  I cannot seem to get this woring on new computer..
+require("lspconfig").clangd.setup(config({
+	cmd = { "clangd", "--background-index", "--log=verbose" },
+    root_dir = function()
+        print("clangd-Rootdir", vim.loop.cwd())
+		return vim.loop.cwd()
+	end,
+}))
+--]]
+require("lspconfig").ccls.setup(config())
+
+require("lspconfig").solang.setup(config())
+
+require("lspconfig").cssls.setup(config())
+
 require'lspconfig'.rust_analyzer.setup {
   on_attach = on_attach,
   settings = {
@@ -22,158 +135,8 @@ require'lspconfig'.rust_analyzer.setup {
   }
 }
 
-vim.lsp.set_log_level("debug")
 
-capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-
--- Setup nvim-cmp.
-cmp = require("cmp")
-source_mapping = {
-	buffer = "[Buffer]",
-	nvim_lsp = "[LSP]",
-	nvim_lua = "[Lua]",
-	cmp_tabnine = "[TN]",
-	path = "[Path]",
-}
-lspkind = require("lspkind")
-require('lspkind').init({
-    with_text = true,
-})
-
-cmp.setup({
-	snippet = {
-		expand = function(args)
-			-- For `vsnip` user.
-			-- vim.fn["vsnip#anonymous"](args.body)
-
-			-- For `luasnip` user.
-			require("luasnip").lsp_expand(args.body)
-
-			-- For `ultisnips` user.
-			-- vim.fn["UltiSnips#Anon"](args.body)
-		end,
-	},
-	mapping = {
-		["<C-u>"] = cmp.mapping.scroll_docs(-4),
-		["<C-d>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete(),
-		["<C-e>"] = cmp.mapping.close(),
-        ['<CR>'] = cmp.mapping.confirm {
-              behavior = cmp.ConfirmBehavior.Replace,
-              select = true,
-            },
-        ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            else
-                fallback()
-            end
-        end, { 'i', 's', }),
-        ['<S-Tab>'] = function(core, fallback)
-          if vim.fn.pumvisible() == 1 then
-            vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-p>', true, true, true), 'n')
-          elseif luasnip.jumpable(-1) then
-            vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
-          else
-            fallback()
-          end
-        end,
-        ['<S-Tab>'] = function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end,
-        },
-
-   formatting = {
-       format = function(entry, vim_item)
-           vim_item.kind = lspkind.presets.default[vim_item.kind]
-           local menu = source_mapping[entry.source.name]
-           if entry.source.name == 'cmp_tabnine' then
-               if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then
-                   menu = entry.completion_item.data.detail .. ' ' .. menu
-               end
-               vim_item.kind = ''
-           end
-           vim_item.menu = menu
-           return vim_item
-       end
-   },
-
-	sources = {
-        -- tabnine completion? yayaya
-        { name = "cmp_tabnine" },
-
-		{ name = "nvim_lsp" },
-
-		-- For vsnip user.
-		-- { name = 'vsnip' },
-
-		-- For luasnip user.
-		{ name = "luasnip" },
-
-		-- For ultisnips user.
-		-- { name = 'ultisnips' },
-
-		{ name = "buffer" },
-	},
-})
-
-tabnine = require('cmp_tabnine.config')
-tabnine:setup({
-    max_lines = 1000,
-    max_num_results = 20,
-    sort = true,
-	run_on_every_keystroke = true,
-	snippet_placeholder = '..',
-})
-
-function config(_config)
-	return vim.tbl_deep_extend("force", {
-		capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
-	}, _config or {})
-end
-
-require("lspconfig").tsserver.setup(config())
-
---[[  I cannot seem to get this woring on new computer..
-require("lspconfig").clangd.setup(config({
-	cmd = { "clangd", "--background-index", "--log=verbose" },
-    root_dir = function()
-        print("clangd-Rootdir", vim.loop.cwd())
-		return vim.loop.cwd()
-	end,
-}))
-require("lspconfig").solang.setup(config())
-
---]]
-
-require("lspconfig").cssls.setup(config())
-
---[[ require("lspconfig").gopls.setup(config({
-	cmd = { "gopls", "serve" },
-	settings = {
-		gopls = {
-			analyses = {
-				unusedparams = true,
-			},
-			staticcheck = true,
-		},
-	},
-}))
---]]
-
-require('lspconfig').csharp_ls.setup{}
-
-require'lspconfig'.pyright.setup{}
-
-
-opts = {
+local opts = {
 	-- whether to highlight the currently hovered symbol
 	-- disable if your cpu usage is higher than you want it
 	-- or you just hate the highlight
@@ -187,7 +150,7 @@ opts = {
 
 require("symbols-outline").setup(opts)
 
-snippets_paths = function()
+local snippets_paths = function()
 	local plugins = { "friendly-snippets" }
 	local paths = {}
 	local path
