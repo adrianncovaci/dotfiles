fcae8778e7b88b3916ed272a3b2a6f0c73f35db1
Adrian <adrianmihaicovaci@gmail.com>
2025-06-09 00:55:40 +0400

    nvim: Add trouble.nvim

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -2,204 +2,241 @@ local overrides = require("custom.configs.overrides")
 
 ---@type NvPluginSpec[]
 local plugins = {
-  {
-    "windwp/nvim-ts-autotag",
-    ft = {
-      "javascript",
-      "javascriptreact",
-      "typescript",
-      "typescriptreact",
-      "html",
-      "svelte",
-      "vue",
-      "xml",
-    },
-    config = function()
-      require("nvim-ts-autotag").setup()
-    end
-  },
-  --
-  -- install dap
-  {
-  "mfussenegger/nvim-dap",
-    dependencies = {
-      "leoluz/nvim-dap-go",
-      "rcarriga/nvim-dap-ui",
-      "tpope/vim-fugitive",
-    },
-    config = function()
-      require("dapui").setup()
-      local dap, dapui = require("dap"), require("dapui")
+	{
+		"windwp/nvim-ts-autotag",
+		ft = {
+			"javascript",
+			"javascriptreact",
+			"typescript",
+			"typescriptreact",
+			"html",
+			"svelte",
+			"vue",
+			"xml",
+		},
+		config = function()
+			require("nvim-ts-autotag").setup()
+		end,
+	},
+	--
+	-- install dap
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"leoluz/nvim-dap-go",
+			"rcarriga/nvim-dap-ui",
+			"tpope/vim-fugitive",
+		},
+		config = function()
+			require("dapui").setup()
+			local dap, dapui = require("dap"), require("dapui")
 
-      dap.listeners.before.attach.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.launch.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated.dapui_config = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited.dapui_config = function()
-        dapui.close()
-      end
-        vim.keymap.set("n", "<leader>dc", dap.continue, {})
-        vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, {})
-    end,
-  },
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
+			end
+			vim.keymap.set("n", "<leader>dc", dap.continue, {})
+			vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, {})
+		end,
+	},
 
- {
-    'saecki/crates.nvim',
-    ft = {"toml"},
-    config = function(_, opts)
-      local crates  = require('crates')
-      crates.setup(opts)
-      require('cmp').setup.buffer({
-        sources = { { name = "crates" }}
-      })
-      crates.show()
-      require("core.utils").load_mappings("crates")
-    end,
-  },
- {
-   'mrcjkb/rustaceanvim',
-   version = '^5',
-   ft = { 'rust' },
-   dependencies = {
-     'nvim-lua/plenary.nvim',
-     'mfussenegger/nvim-dap',
-     'akinsho/toggleterm.nvim',
-   },
-   config = function()
-     vim.g.rustaceanvim = {
-       tools = {
-         executor = 'toggleterm',
-         hover_actions = {
-           auto_focus = true,
-         },
-         inlay_hints = {
-           auto = true,
-         },
-       },
-       server = {
-         on_attach = function(client, bufnr)
-           require("plugins.configs.lspconfig").on_attach(client, bufnr)
-         end,
-         default_settings = {
-           ['rust-analyzer'] = {
-             cargo = {
-               allFeatures = true,
-               loadOutDirsFromCheck = true,
-               runBuildScripts = true,
-             },
-             checkOnSave = {
-               command = "clippy",
-               allFeatures = true,
-               enable = true,
-             },
-             procMacro = {
-               enable = true,
-             },
-             inlayHints = {
-               lifetimeElisionHints = {
-                 enable = true,
-                 useParameterNames = true,
-               },
-             },
-             diagnostics = {
-               enable = true,
-               experimental = {
-                 enable = true,
-               },
-             },
-           },
-         },
-       },
-     }
+	{
+		"saecki/crates.nvim",
+		ft = { "toml" },
+		config = function(_, opts)
+			local crates = require("crates")
+			crates.setup(opts)
+			require("cmp").setup.buffer({
+				sources = { { name = "crates" } },
+			})
+			crates.show()
+			require("core.utils").load_mappings("crates")
+		end,
+	},
+	{
+		"mrcjkb/rustaceanvim",
+		version = "^5",
+		ft = { "rust" },
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"mfussenegger/nvim-dap",
+			"akinsho/toggleterm.nvim",
+		},
+		config = function()
+			vim.g.rustaceanvim = {
+				tools = {
+					executor = "toggleterm",
+					hover_actions = {
+						auto_focus = true,
+					},
+					inlay_hints = {
+						auto = true,
+					},
+				},
+				server = {
+					on_attach = function(client, bufnr)
+						require("plugins.configs.lspconfig").on_attach(client, bufnr)
+					end,
+					default_settings = {
+						["rust-analyzer"] = {
+							cargo = {
+								allFeatures = true,
+								loadOutDirsFromCheck = true,
+								runBuildScripts = true,
+							},
+							checkOnSave = {
+								command = "clippy",
+								allFeatures = true,
+								enable = true,
+							},
+							procMacro = {
+								enable = true,
+							},
+							inlayHints = {
+								lifetimeElisionHints = {
+									enable = true,
+									useParameterNames = true,
+								},
+							},
+							diagnostics = {
+								enable = true,
+								experimental = {
+									enable = true,
+								},
+							},
+						},
+					},
+				},
+			}
 
-     -- Ensure diagnostics are enabled and configured
-     vim.diagnostic.config({
-       virtual_text = true,
-       signs = true,
-       underline = true,
-       update_in_insert = false,
-       severity_sort = false,
-     })
-   end
- },
-  {
-    "github/copilot.vim",
-    lazy = false,
-    config = function()
-      -- Mapping tab is already used by NvChad
-      vim.g.copilot_no_tab_map = true;
-      vim.g.copilot_assume_mapped = true;
-      vim.g.copilot_tab_fallback = "\t";
-      -- The mapping is set to other key, see custom/lua/mappings
-      -- or run <leader>ch to see copilot mapping section
-    end
-  },
-  -- Override plugin definition options
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end, -- Override to setup mason-lspconfig
-  },
+			-- Ensure diagnostics are enabled and configured
+			vim.diagnostic.config({
+				virtual_text = true,
+				signs = true,
+				underline = true,
+				update_in_insert = false,
+				severity_sort = false,
+			})
+		end,
+	},
+	{
+		"github/copilot.vim",
+		lazy = false,
+		config = function()
+			-- Mapping tab is already used by NvChad
+			vim.g.copilot_no_tab_map = true
+			vim.g.copilot_assume_mapped = true
+			vim.g.copilot_tab_fallback = "\t"
+			-- The mapping is set to other key, see custom/lua/mappings
+			-- or run <leader>ch to see copilot mapping section
+		end,
+	},
+	-- Override plugin definition options
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.configs.lspconfig")
+		end, -- Override to setup mason-lspconfig
+	},
 
-  -- override plugin configs
-  {
-    "williamboman/mason.nvim",
-    opts = overrides.mason
-  },
+	-- override plugin configs
+	{
+		"williamboman/mason.nvim",
+		opts = overrides.mason,
+	},
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = overrides.treesitter,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = overrides.treesitter,
+	},
 
-  {
-    "nvim-tree/nvim-tree.lua",
-    opts = overrides.nvimtree,
-  },
+	{
+		"nvim-tree/nvim-tree.lua",
+		opts = overrides.nvimtree,
+	},
 
-  -- Install a plugin
-  {
-    "max397574/better-escape.nvim",
-    event = "InsertEnter",
-    config = function()
-      require("better_escape").setup()
-    end,
-  },
+	-- Install a plugin
+	{
+		"max397574/better-escape.nvim",
+		event = "InsertEnter",
+		config = function()
+			require("better_escape").setup()
+		end,
+	},
 
-  {
-    "stevearc/conform.nvim",
-    --  for users those who want auto-save conform + lazyloading!
-    -- event = "BufWritePre"
-    config = function()
-      require "custom.configs.conform"
-    end,
-  },
-  { "nvim-neotest/nvim-nio" },
-  {
-    "vuciv/golf",
-    lazy = false,
-  }
+	{
+		"stevearc/conform.nvim",
+		--  for users those who want auto-save conform + lazyloading!
+		-- event = "BufWritePre"
+		config = function()
+			require("custom.configs.conform")
+		end,
+	},
+	{ "nvim-neotest/nvim-nio" },
+	{
+		"vuciv/golf",
+		lazy = false,
+	},
+	{
+		"folke/trouble.nvim",
+		opts = {}, -- for default options, refer to the configuration section for custom setup.
+		cmd = "Trouble",
+		keys = {
+			{
+				"<leader>fx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>fX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>fL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>fQ",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
+		},
+	},
 
-  -- To make a plugin not be loaded
-  -- {
-  --   "NvChad/nvim-colorizer.lua",
-  --   enabled = false
-  -- },
+	-- To make a plugin not be loaded
+	-- {
+	--   "NvChad/nvim-colorizer.lua",
+	--   enabled = false
+	-- },
 
-  -- All NvChad plugins are lazy-loaded by default
-  -- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
-  -- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
-  -- {
-  --   "mg979/vim-visual-multi",
-  --   lazy = false,
-  -- }
+	-- All NvChad plugins are lazy-loaded by default
+	-- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
+	-- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
+	-- {
+	--   "mg979/vim-visual-multi",
+	--   lazy = false,
+	-- }
 }
 
 return plugins
